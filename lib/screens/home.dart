06cd896033ba27540c21b53b06380f8a0e9ac066
Saurabh Kumar <saurabh09.ind@gmail.com>
2023-07-13 02:46:50 +0530

    searchBar added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,7 +10,33 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _BuildAppBar(),
       body: Container(
-        child: Text('Building a platform to provide career guidance'),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              searchBox(),
+            ],
+          )),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
       ),
     );
   }
