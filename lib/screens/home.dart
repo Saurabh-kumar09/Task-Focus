602f2556b7bc045ba339e8bd47154e0d8f0bc76b
Saurabh Kumar <saurabh09.ind@gmail.com>
2023-07-13 02:28:58 +0530

    appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,12 +7,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Career Insight'),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _BuildAppBar(),
       body: Container(
         child: Text('Building a platform to provide career guidance'),
       ),
     );
+  }
+
+  AppBar _BuildAppBar() {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                        'build/flutter_assets/images/profile_pic.jpg')))
+          ],
+        ));
   }
 }
